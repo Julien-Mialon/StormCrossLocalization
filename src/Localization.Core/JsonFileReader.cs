27efab9d5939ae659a924fceed33d4225ca1b180
Julien Mialon <mialon.julien@gmail.com>
2017-10-03 22:17:13 +0200

    Implement support for json resource file

---
+++ src/Localization.Core/JsonFileReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Localization.Core
+{
+	public class JsonFileReader : IFileReader
+	{
+		public bool HasSupportForFile(string file)
+		{
+			string extension = Path.GetExtension(file)?.TrimStart('.').ToLowerInvariant() ?? string.Empty;
+			return extension == "json";
+		}
+
+		public Dictionary<string, string> ReadResourceFile(string file)
+		{
+			JObject result = JToken.Parse(File.ReadAllText(file)) as JObject;
+			if (result == null)
+			{
+				return new Dictionary<string, string>();
+			}
+
+			return result.Properties().ToDictionary(x => x.Name, x => x.Value.ToString());
+		}
+	}
+}
